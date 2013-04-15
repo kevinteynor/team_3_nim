4ea6886f8cc83a8bab7b66a453dd72a435c819b6
Justin Ward <jward@student.neumont.edu>
2013-04-15 14:06:54 -0600

    Added Refresh() to NimLearningAI.cs. Update ChangeList.txt.

--- NIM project/LearningNimGame/NimLearningAI.cs
+++ NIM project/LearningNimGame/NimLearningAI.cs
@@ -7,6 +7,26 @@ namespace LearningNimGame
 {
     public class NimLearningAI
     {
+        private int GamesPlayed = 0;
+        List<BoardState> BoardStateCatalog;
 
+        public NimLearningAI()
+        {
+            Refresh();
+        }
+
+        /// <summary>
+        /// Resents the AI's BoardStateCatalog and GamesPlayed, reseting it to a
+        /// completely "dumb" state.
+        /// </summary>
+        public void Refresh()
+        {
+            BoardStateCatalog = new List<BoardState>();
+
+            foreach (var b in NimLogic.EveryPossibleBoardState())
+                BoardStateCatalog.Add(b);
+
+            GamesPlayed = 0;
+        }
     }
 }
